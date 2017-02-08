c91b64462a1ef63ce1b9dd4bad1d7e51342d7251
M-Hawkins <matthew.alexander.hawkins@gmail.com>
2017-02-07 22:48:29 -0500

    Added comments and skeleton code for some basic helper functions

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,24 +1,32 @@
 #include <SPI.h>
 
-//SCK  = 13
-//MISO = 12
-//MOSI = 11
-//SS are active low
+// SCK  = 13
+// MISO = 12
+// MOSI = 11
 
+// Initialize pin numbers and constants
 const int X_SS = 8;
 const int Y_SS = 9;
 const int Z_SS = 10;
 const int MAX_TIMEOUT_ATTEMPTS = 1000;
 
+// Initialize variables
 int xPos = 0;
 int yPos = 0;
 int zPos = 0;
 int numAttempts = 0;
 
 void setup() {
+  // Configure slave select pins as outputs
   pinMode(X_SS, OUTPUT);
   pinMode(Y_SS, OUTPUT);
   pinMode(Z_SS, OUTPUT);
+
+  // Disable all stages
+  digitalWrite(X_SS, HIGH);
+  digitalWrite(Y_SS, HIGH);
+  digitalWrite(Z_SS, HIGH);
+
   SPI.begin();
   delay(100);
 }
@@ -28,16 +36,44 @@ void loop() {
 
 }
 
-//"when fetching  a reply from  the M3‐LS,  it  is  recommended that  each  SPI 
-//exchange  be  spaced  by  60  µsec  in  order to  guarantee that  duplicate bytes are not received"
-
+// Gets and stores the current X, Y, and Z position of the stages
 void getCurrentPosition(){
-  
+  xPos = getAxisPosition(X_SS);
+  yPos = getAxisPosition(Y_SS);
+  zPos = getAxisPosition(Z_SS);
+}
+
+// Get the current position of a single stage
+int getAxisPosition(int pin){
+  digitalWrite(pin, LOW);
+  sendSPICommand(10);
+  waitForResponse();
+  // TODO
+  // Read response into var to return
+  // 8 bytes at a time
+  digitalWrite(pin, HIGH);
+  return 0;
 }
 
 void moveToTargetPosition(){
-  
+  return;
 }
 
-//...and anything else you can think of
+// Helper function which waits for a response over SPI
+void waitForResponse(){
+  // TODO
+  // Check until a 1 is not returned
+  // May have to return this value
+  return;
+}
 
+// Helper function used to send SPI commands specific to the M3-LS
+void sendSPICommand(int commandNumber){
+  // Build command as a string
+  String commandString = "<" + String(commandNumber) + ">\r";
+
+  // Transmit the string over SPI, one char at a time
+  for (unsigned int i = 0; i < commandString.length(); i++){
+    SPI.transfer(commandString[i]);
+  }
+}
