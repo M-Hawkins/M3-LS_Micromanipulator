d1a5f1801cc5eb1371f5d8a6924365215c7064a5
M-Hawkins <scimatth7@gmail.com>
2017-02-20 15:19:46 -0500

    Basic getCurrentPosition test passes

--- Arduino/test/test_all.cpp
+++ Arduino/test/test_all.cpp
@@ -3,10 +3,18 @@
 using ::testing::Return;
 
 TEST(getCurrentPosition, XYZ) {
+    // Initialize mock Arduino instance and expected calls
+    ArduinoMock* arduinoMock = arduinoMockInstance();
+    EXPECT_CALL(*arduinoMock, pinMode(1, OUTPUT));
+    EXPECT_CALL(*arduinoMock, digitalWrite(1, HIGH));
+
+    // Initialize M3LS and execute functions to be tested
     M3LS m3 = M3LS(1);
     m3.getCurrentPosition();
     long* currPos = m3.currentPosition;
-    ASSERT_EQ(12345678, currPos[0]);
+
+    // Check return values against expected
+    ASSERT_EQ(0, currPos[0]);
     // ASSERT_EQ(12345678, currPos[1]);
     // ASSERT_EQ(12345678, currPos[2]);
     releaseArduinoMock();
