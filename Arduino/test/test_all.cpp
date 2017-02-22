c3790dcc56e8a4cd81564393724c59d3c4f07af1
M-Hawkins <scimatth7@gmail.com>
2017-02-22 17:35:19 -0500

    Created execution time tests

--- Arduino/test/test_all.cpp
+++ Arduino/test/test_all.cpp
@@ -1,6 +1,35 @@
 #include "gtest/gtest.h"
 #include "M3LS.h"
+#include <sys/time.h>
 using ::testing::Return;
+
+namespace testing
+{
+ namespace internal
+ {
+  enum GTestColor {
+      COLOR_DEFAULT,
+      COLOR_RED,
+      COLOR_GREEN,
+      COLOR_YELLOW
+  };
+
+  extern void ColoredPrintf(GTestColor color, const char* fmt, ...);
+ }
+}
+#define PRINTF(...)  do { testing::internal::ColoredPrintf(testing::internal::COLOR_GREEN, "[          ] "); testing::internal::ColoredPrintf(testing::internal::COLOR_YELLOW, __VA_ARGS__); } while(0)
+
+// C++ stream interface
+class TestCout : public std::stringstream
+{
+public:
+    ~TestCout()
+    {
+        PRINTF("%s",str().c_str());
+    }
+};
+
+#define TEST_COUT  TestCout()
 
 TEST(Constructor, SingleAxis){
     // Initialize test parameters
@@ -367,5 +396,75 @@ TEST(MoveToTargetPosition, XYZ){
     ASSERT_EQ(startingPositions[1], currPos[1]);
     ASSERT_EQ(startingPositions[2], currPos[2]);
 
+    releaseArduinoMock();
+}
+
+TEST(ExecutionTime, GetCurrentPosition){
+    // Initialize test parameters and call test function
+    int pins[3] = {1, 2, 3};
+    int numAxes = 3;
+    long startingPositions[3] = {11111234L, 22221234L, 33331234L};
+    
+    // Initialize mock Arduino instance and expected calls
+    ArduinoMock* arduinoMock = arduinoMockInstance();
+    for (int pin = 0; pin < numAxes; pin++){
+        EXPECT_CALL(*arduinoMock, pinMode(pins[pin], OUTPUT));
+        EXPECT_CALL(*arduinoMock, digitalWrite(pins[pin], HIGH));
+    }
+
+    // Initialize M3LS with starting positions
+    M3LS m3 = M3LS(pins[0], pins[1], pins[2]);
+    for (int pin = 0; pin < numAxes; pin++){
+        m3.currentPosition[pin] = startingPositions[pin];
+    }
+
+    // Time 100k executions of a function
+    struct timeval stop, start;
+    gettimeofday(&start, NULL);
+    long* currPos;
+    for (int i = 0; i < 100000; i++){
+        currPos = m3.getCurrentPosition();
+    }
+    gettimeofday(&stop, NULL);
+    long unsigned timeDiff = stop.tv_usec - start.tv_usec;
+
+    // Calculate and print out average execution time
+    PRINTF("Average execution time is %f us\n", timeDiff / 100000.0);
+    releaseArduinoMock();
+}
+
+TEST(ExecutionTime, MoveToTargetPosition){
+    // Initialize test parameters and call test function
+    int pins[3] = {1, 2, 3};
+    int numAxes = 3;
+    long startingPositions[3] = {11111234L, 22221234L, 33331234L};
+    long targetPositionX = 12344444L;
+    long targetPositionY = 12345555L;
+    long targetPositionZ = 12346666L;
+    
+    // Initialize mock Arduino instance and expected calls
+    ArduinoMock* arduinoMock = arduinoMockInstance();
+    for (int pin = 0; pin < numAxes; pin++){
+        EXPECT_CALL(*arduinoMock, pinMode(pins[pin], OUTPUT));
+        EXPECT_CALL(*arduinoMock, digitalWrite(pins[pin], HIGH));
+    }
+
+    // Initialize M3LS with starting positions
+    M3LS m3 = M3LS(pins[0], pins[1], pins[2]);
+    for (int pin = 0; pin < numAxes; pin++){
+        m3.currentPosition[pin] = startingPositions[pin];
+    }
+
+    // Time 100k executions of a function
+    struct timeval stop, start;
+    gettimeofday(&start, NULL);
+    for (int i = 0; i < 100000; i++){
+        m3.moveToTargetPosition(targetPositionX, targetPositionY, targetPositionZ);
+    }
+    gettimeofday(&stop, NULL);
+    long unsigned timeDiff = stop.tv_usec - start.tv_usec;
+
+    // Calculate and print out average execution time
+    PRINTF("Average execution time is %f us\n", timeDiff / 100000.0);
     releaseArduinoMock();
 }
