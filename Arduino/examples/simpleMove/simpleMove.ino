d942843673d052e09451bde1ade9d424f7b063dd
Colby Rome <cdr013@bucknell.edu>
2017-03-02 14:02:25 -0500

    Started to write simpleMove

---
+++ Arduino/examples/simpleMove/simpleMove.ino
@@ -0,0 +1,14 @@
+#include "M3LS.h"
+
+int xpin = 4;
+
+M3LS myM3LS(xpin);
+
+void setup(){
+    // Serial is already setup
+    Serial.println("Testing M3LS Movement");
+}
+
+void loop(){
+    myM3LS.
+}
