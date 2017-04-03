20714279ae96ef65748ff4bf0df78d81aa6d43c5
Colby Rome <cdr013@bucknell.edu>
2017-04-02 21:15:50 -0400

    actually fixed scalefactor bug'

--- C++/src/M3LS.cc
+++ C++/src/M3LS.cc
@@ -169,7 +169,7 @@ void M3LS::updatePosition(int inp0, int inp1, int inp2, Axes axis, bool isActive
 
                         // Loop through each available axis
                         for (int axis = 0; axis < numAxes; axis++){
-                            int inp = map(inputs[axis], 0, 255, -((numZones - 1) / 2) * scaleFactor, ((numZones - 1) / 2)) * scaleFactor;
+                            int inp = map(inputs[axis], 0, 255, -((numZones - 1) / 2), ((numZones - 1) / 2)) * scaleFactor;
                             advanceMotor(inp, axis);
                         }
 
