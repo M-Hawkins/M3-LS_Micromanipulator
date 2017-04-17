0418ba0799ed7a5b8b0aeac049adb3ad31e9886f
M-Hawkins <matthew.alexander.hawkins@gmail.com>
2017-04-17 16:07:17 -0400

    Added tests for ToggleHold

--- C++/test/test_all.cpp
+++ C++/test/test_all.cpp
@@ -20,7 +20,7 @@ TEST(Constructor, SingleAxis){
     }
     M3LS m3 = M3LS(pins[0]);
 
-    // Set up exected calls for begin
+    // Set up expected calls for begin
     EXPECT_CALL(*arduinoMock, delay(250)).Times(4);
     m3.begin();
 
@@ -47,7 +47,7 @@ TEST(Constructor, DoubleAxis){
     }
     M3LS m3 = M3LS(pins[0], pins[1]);
 
-    // Set up exected calls for begin
+    // Set up expected calls for begin
     EXPECT_CALL(*arduinoMock, delay(250)).Times(4);
     m3.begin();
 
@@ -74,9 +74,69 @@ TEST(Constructor, TripleAxis){
     }
     M3LS m3 = M3LS(pins[0], pins[1], pins[2]);
 
-    // Set up exected calls for begin
+    // Set up expected calls for begin
     EXPECT_CALL(*arduinoMock, delay(250)).Times(4);
     m3.begin();
+
+    // Cleanup mock
+    releaseArduinoMock();
+    releaseSPIMock();
+}
+
+TEST(ToggleHold, SetHold){
+    // Initialize test parameters
+    int pins[] = {1, 2, 3};
+    int numAxes = 3;
+
+    // Initialize mock Arduino and SPI
+    ArduinoMock* arduinoMock = arduinoMockInstance();
+    SPIMock* spiMock = SPIMockInstance();
+
+    // Set up expected calls for constructor
+    EXPECT_CALL(*arduinoMock, delay(50));
+    EXPECT_CALL(*spiMock, begin());
+    for (int pin = 0; pin < numAxes; pin++){
+        EXPECT_CALL(*arduinoMock, pinMode(pins[pin], OUTPUT));
+        EXPECT_CALL(*arduinoMock, digitalWrite(pins[pin], HIGH));
+    }
+    M3LS m3 = M3LS(pins[0], pins[1], pins[2]);
+
+    // Set up expected calls for begin
+    EXPECT_CALL(*arduinoMock, delay(250)).Times(4);
+    m3.begin();
+
+    // Set up expected calls for setting the control mode
+    m3.setControlMode(M3LS::hold);
+
+    // Cleanup mock
+    releaseArduinoMock();
+    releaseSPIMock();
+}
+
+TEST(ToggleHold, SetPosition){
+    // Initialize test parameters
+    int pins[] = {1, 2, 3};
+    int numAxes = 3;
+
+    // Initialize mock Arduino and SPI
+    ArduinoMock* arduinoMock = arduinoMockInstance();
+    SPIMock* spiMock = SPIMockInstance();
+
+    // Set up expected calls for constructor
+    EXPECT_CALL(*arduinoMock, delay(50));
+    EXPECT_CALL(*spiMock, begin());
+    for (int pin = 0; pin < numAxes; pin++){
+        EXPECT_CALL(*arduinoMock, pinMode(pins[pin], OUTPUT));
+        EXPECT_CALL(*arduinoMock, digitalWrite(pins[pin], HIGH));
+    }
+    M3LS m3 = M3LS(pins[0], pins[1], pins[2]);
+
+    // Set up expected calls for begin
+    EXPECT_CALL(*arduinoMock, delay(250)).Times(4);
+    m3.begin();
+
+    // Set up expected calls for setting the control mode
+    m3.setControlMode(M3LS::position);
 
     // Cleanup mock
     releaseArduinoMock();
