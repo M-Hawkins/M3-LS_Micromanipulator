f4af7771955b056a89bd9add0ed69a85c9c73ad1
M-Hawkins <matthew.alexander.hawkins@gmail.com>
2017-04-17 15:51:10 -0400

    Reworked all constructor tests

--- C++/test/test_all.cpp
+++ C++/test/test_all.cpp
@@ -21,7 +21,7 @@ TEST(Constructor, SingleAxis){
     M3LS m3 = M3LS(pins[0]);
 
     // Set up exected calls for begin
-    EXPECT_CALL(*arduinoMock, delay(250)).Times(numAxes * 4);
+    EXPECT_CALL(*arduinoMock, delay(250)).Times(4);
     m3.begin();
 
     // Cleanup mock
@@ -29,38 +29,56 @@ TEST(Constructor, SingleAxis){
     releaseSPIMock();
 }
 
-// TEST(Constructor, DoubleAxis){
-//     // Initialize test parameters
-//     int pins[] = {1, 2};
-//     int numAxes = 2;
+TEST(Constructor, DoubleAxis){
+    // Initialize test parameters
+    int pins[] = {1, 2};
+    int numAxes = 2;
 
-//     // Initialize mock Arduino instance and expected calls
-//     ArduinoMock* arduinoMock = arduinoMockInstance();
-//     for (int pin = 0; pin < numAxes; pin++){
-//         EXPECT_CALL(*arduinoMock, pinMode(pins[pin], OUTPUT));
-//         EXPECT_CALL(*arduinoMock, digitalWrite(pins[pin], HIGH));
-//     }
+    // Initialize mock Arduino and SPI
+    ArduinoMock* arduinoMock = arduinoMockInstance();
+    SPIMock* spiMock = SPIMockInstance();
 
-//     // Initialize M3LS and execute functions to be tested
-//     M3LS m3 = M3LS(pins[0], pins[1]);
-//     m3.begin();
-//     releaseArduinoMock();
-// }
+    // Set up expected calls for constructor
+    EXPECT_CALL(*arduinoMock, delay(50));
+    EXPECT_CALL(*spiMock, begin());
+    for (int pin = 0; pin < numAxes; pin++){
+        EXPECT_CALL(*arduinoMock, pinMode(pins[pin], OUTPUT));
+        EXPECT_CALL(*arduinoMock, digitalWrite(pins[pin], HIGH));
+    }
+    M3LS m3 = M3LS(pins[0], pins[1]);
 
-// TEST(Constructor, TripleAxis){
-//     // Initialize test parameters
-//     int pins[] = {1, 2, 3};
-//     int numAxes = 3;
+    // Set up exected calls for begin
+    EXPECT_CALL(*arduinoMock, delay(250)).Times(4);
+    m3.begin();
 
-//     // Initialize mock Arduino instance and expected calls
-//     ArduinoMock* arduinoMock = arduinoMockInstance();
-//     for (int pin = 0; pin < numAxes; pin++){
-//         EXPECT_CALL(*arduinoMock, pinMode(pins[pin], OUTPUT));
-//         EXPECT_CALL(*arduinoMock, digitalWrite(pins[pin], HIGH));
-//     }
+    // Cleanup mock
+    releaseArduinoMock();
+    releaseSPIMock();
+}
 
-//     // Initialize M3LS and execute functions to be tested
-//     M3LS m3 = M3LS(pins[0], pins[1], pins[2]);
-//     m3.begin();
-//     releaseArduinoMock();
-// }
+TEST(Constructor, TripleAxis){
+    // Initialize test parameters
+    int pins[] = {1, 2, 3};
+    int numAxes = 3;
+
+    // Initialize mock Arduino and SPI
+    ArduinoMock* arduinoMock = arduinoMockInstance();
+    SPIMock* spiMock = SPIMockInstance();
+
+    // Set up expected calls for constructor
+    EXPECT_CALL(*arduinoMock, delay(50));
+    EXPECT_CALL(*spiMock, begin());
+    for (int pin = 0; pin < numAxes; pin++){
+        EXPECT_CALL(*arduinoMock, pinMode(pins[pin], OUTPUT));
+        EXPECT_CALL(*arduinoMock, digitalWrite(pins[pin], HIGH));
+    }
+    M3LS m3 = M3LS(pins[0], pins[1], pins[2]);
+
+    // Set up exected calls for begin
+    EXPECT_CALL(*arduinoMock, delay(250)).Times(4);
+    m3.begin();
+
+    // Cleanup mock
+    releaseArduinoMock();
+    releaseSPIMock();
+}
